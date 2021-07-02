61ef119e8b65ebc15af932872a4a15396012e78f
A7abhilash <a7abhilash@gmail.com>
2021-07-02 19:59:47 +0530

    removed unwanted npm packages and unused codes

--- server/server.js
+++ server/server.js
@@ -2,16 +2,9 @@ const express = require("./node_modules/express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const mongoose = require("mongoose");
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema } = require("graphql");
-const RootQueryType = require("./schemas/RootQueryType");
-const RootMutationType = require("./schemas/RootMutationType");
 const schema = require("./graphql/schema");
 const resolvers = require("./graphql/resolvers");
-const Users = require("./models/Users");
 
 const app = express();
 
@@ -22,27 +15,11 @@ dotenv.config({ path: "./config/config.env" });
 //database
 connectDB();
 
-//graphql
-// const schema = new GraphQLSchema({
-//   query: RootQueryType,
-//   mutation: RootMutationType,
-// });
-
 //*****MIDDLEWARE
 //Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-//Session
-app.use(
-  session({
-    secret: "7781",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
-);
 
 //Routes
 app.use(
